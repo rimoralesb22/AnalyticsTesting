b68b9d704f6ef6e3380eab2c2a4416965f117d7f
isaaclpez <isaaclpez@gmail.com>
2016-04-21 14:09:36 -0600

    Con variables estandarizadas
    Luego de la ayuda de Yenny

--- Proyecto Avon/K-means sample rows.r
+++ Proyecto Avon/K-means sample rows.r
@@ -6,7 +6,7 @@ source('utils.R')
 # install the xlsx package, if needed
 # and load it into memory
 ensure.loaded("dplyr")
-
+ensure.loaded("cluster")
 #Kmeans
 
 #rs <- dbSendQuery(con,"select vf.cuenta,sum(vf.precio_folleto1) AS Ganacia,
@@ -15,26 +15,65 @@ ensure.loaded("dplyr")
 #                  from avon.view_Factura2013_2015_3 vf 
 #                  group by vf.cuenta")
 
+
+#Funcion para outliers
+remove_outliers <- function(x, na.rm = TRUE, ...) {
+  qnt <- quantile(x, probs=c(.001, .999), na.rm = na.rm, ...)
+  H <- 1.5 * IQR(x, na.rm = na.rm)
+  y <- x
+  y[x < (qnt[1] - H)] <- NA
+  y[x > (qnt[2] + H)] <- NA
+  y
+} 
+
 #ClientesTot <- fetch(rs)
 ClientesTot = read.csv("Valor_clientes.csv")
 #Clientes$Grupo <- NULL
 head(ClientesTot)
-#Clientes<- transform(Clientes,NOMBRE = as.factor(Clientes$NOMBRE))
+ClientesTot$CUENTA <- gsub(",","",ClientesTot$CUENTA)
+ClientesTot$GANACIA <- gsub(",","",ClientesTot$GANACIA)
+ClientesTot$TOTALCAMPANAS <- gsub(",","",ClientesTot$TOTALCAMPANAS)
+ClientesTot$PARTICIPACION <- gsub(",","",ClientesTot$PARTICIPACION)
 
-Clientes <- sample_n(Clientes, 1000)
+
+#Clientes<- transform(Clientes,NOMBRE = as.factor(Clientes$NOMBRE))
+ClientesTot$CUENTA <- as.numeric(ClientesTot$CUENTA)
+ClientesTot$GANACIA <- as.numeric(ClientesTot$GANACIA)
+ClientesTot$TOTALCAMPANAS <- as.numeric(ClientesTot$TOTALCAMPANAS)
+ClientesTot$PARTICIPACION <- as.numeric(ClientesTot$PARTICIPACION)
+
+#Estandarizar y Escalar las variables y centrar
+Clientes2 = scale(ClientesTot[2:4], center = TRUE, scale = TRUE)
+
+#Sacar los outliers
+Clientes3 <- remove_outliers(Clientes2)
+head(Clientes3)
+
+#Unimos a la matriz la informaciÃ³n original para mantener las variables originales, sin embargo no las tomamos en cuenta para hacer el kmeans.
+
+Clientes5 <- cbind(Clientes3,CUENTA=ClientesTot$CUENTA,GANANCIAREAL=ClientesTot$GANACIA,CAMAPANASREAL=ClientesTot$TOTALCAMPANAS,PARTICIPACIONREAL=ClientesTot$PARTICIPACION)
+Clientes5 <- data.frame(Clientes5)
+head(Clientes5)
+
+
+#Sacar la muestra
+Clientes4 <- sample_n(na.omit(Clientes5), 10000)
+
+
 #Calcular la cantidad de clusters
 #------------------------------------------------------------------------------------
 # Crea vector "Errores", sin datos
 # Crea variable "K_Max" con la cant. maxima de k a analizar
 Errores <-NULL
-K_Max   <-9
+  K_Max   <-9
+
 
 #------------------------------------------------------------------------------------
 # Ejecuta kmeans con diferentes cluster, desde 1 hasta 9
 # Luego guarda el error de cada ejecucion en el vector "Errores"
 for (i in 1:K_Max)
 {
-  Errores[i] <- sum(kmeans(Clientes[2:4], centers=i)$withinss)
+  Errores[i] <- sum(kmeans(Clientes4[1:3], centers=i)$withinss)
 }
 
 #------------------------------------------------------------------------------------
@@ -77,7 +116,7 @@ for (i in 1:CantidadAlgoritmos)
 {
   for (ii in 1:100) 
   {
-    Modelo      <- kmeans(Clientes[2:4],4, algorithm = Algoritmos[i])
+    Modelo      <- kmeans(Clientes4[1:3],5, algorithm = Algoritmos[i])
     Iteraciones <- rbind(Iteraciones,
                          data.frame(Intraclase = Modelo$betweenss,
                                     Algoritmo = Algoritmos[i]))
@@ -93,15 +132,54 @@ AlgoritmoGanador <-names(Resultados[1])
 
 #-------------------------------------------------------------------------
 # PASO 5: Ejecuta kmeans con algoritmo ganador y asigna grupo a cada cliente
-KmeansOptimizado <- kmeans(Clientes[2:4],4, algorithm = AlgoritmoGanador)
-Clientes$Grupo   <-KmeansOptimizado$cluster
+KmeansOptimizado <- kmeans(Clientes4[1:3],5, algorithm = AlgoritmoGanador)
+Clientes4$Grupo   <-KmeansOptimizado$cluster
 
 #-------------------------------------------------------------------------
 
 #PASO 6: Grafica segmentacion de algoritomo ganador y luego asigna etiquetas
-plot(Clientes$TOTALCAMPANAS,Clientes$GANACIA,col=Clientes$Grupo,cex.axis=.7,cex.lab=.7)
-text(Clientes$TOTALCAMPANAS,Clientes$GANANCIA,
-     labels=Clientes$CUENTA,pos=1,col=Clientes$Grupo,cex=.7)
+plot(Clientes4$TOTALCAMPANAS,Clientes4$GANACIA,col=Clientes4$Grupo,cex.axis=.7,cex.lab=.7)
+text(Clientes4$TOTALCAMPANAS,Clientes4$GANACIA,
+     labels=Clientes4$CUENTA,pos=1,col=Clientes4$Grupo,cex=.7)
 title(main=paste("Algoritmo ganador:",AlgoritmoGanador),cex.main=.9)
 
-write.csv(Clientes,"C:/Users/ricmorales/Desktop/BD/ganancia_optimizado_6Clusters_sample.csv")
+write.csv(Clientes4,"C:/Users/isalopez/Desktop/ganancia_5Clusters_centrado.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+#-------------------------------------------------------------------------
+#PAM 
+PAM = pam(Clientes,k=4,metric="euclidean")
+plot(PAM)
+write.csv(PAM,"C:/Users/isalopez/Desktop/ganancia_optimizado_6Clusters_sample.csv")
+
+#-------------------------------------------------------------------------
+#ACP
+install.packages("FactoMineR")
+library(FactoMineR)
+pruebapca.pca=PCA(Clientes,graph=T)
+
+install.packages("ade4")
+library(ade4)
+
+coordenadas=pruebapca.pca$ind$coord
+#PLANO PRINCIPAL
+s.label(coordenadas,xax = 1,yax = 2,label=Clientes[,1],possub="bottomleft")
+
+library(amap)
+
+#Prueba
+Clientes2=scale(Clientes[2:4],center = T,scale = T)
+prueba.pca=princomp(Clientes2)
+comp=predict(prueba.pca)[,1:2]
+grupos=kmeans(comp,4)
+plot(comp,col=grupos$cluster)
